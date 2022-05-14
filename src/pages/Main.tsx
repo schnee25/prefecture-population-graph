c03ea209e38450af32b19f9a3eb47e79f2410372
schnee25 <schnee.2114@gmail.com>
2022-05-14 21:44:27 +0900

    CSS別シートに&文字の間隔の調整

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,3 +1,4 @@
+import styles from "./style.module.scss";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PrefecturesList from "../Components/PrefectureList/PrefecturesList";
@@ -7,18 +8,6 @@ import Graph from "../Components/Graph/Graph";
 const ApiKey = "owZHJwNQSeb7WDoUgRRiAWlJRXEhqP6Vr5NcYO8W";
 
 console.log("aaa", ApiKey);
-
-const Styles: { [key: string]: React.CSSProperties } = {
-  graph: {
-    padding: "10px",
-  },
-  label: {
-    fontSize: "20px",
-    padding: "0.5rem 2rem",
-    borderLeft: "4px solid #000",
-    marginLeft: "10pt",
-  },
-};
 
 const Main: React.FC = () => {
   const [prefectures, setPreFectures] = useState<{
@@ -84,11 +73,11 @@ const Main: React.FC = () => {
 
   return (
     <main>
-      <h2 style={Styles.label}>都道府県</h2>
+      <h2 className={styles["label"]}>都道府県</h2>
       {prefectures && (
         <PrefecturesList prefectures={prefectures.result} onChange={handleClickCheck} />
       )}
-      <h2 style={Styles.label}>人口推移グラフ</h2>
+      <h2 className={styles["label"]}>人口推移グラフ</h2>
       <Graph populationData={prefPopulation} />
     </main>
   );
