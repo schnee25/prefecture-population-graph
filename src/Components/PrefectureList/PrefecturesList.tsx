d6615f543f720f703fb5ed0bc1c5ed78235eee3a
schnee25 <schnee.2114@gmail.com>
2022-05-14 23:20:24 +0900

    チェックボックスの状態が保持されるよう演算子の使用場所を修正

--- src/Components/PrefectureList/PrefecturesList.tsx
+++ src/Components/PrefectureList/PrefecturesList.tsx
@@ -15,39 +15,52 @@ type Props = {
 // 都道府県一覧のチェックボックスを表示するコンポーネント
 const PrefecturesList: React.FC<Props> = ({ prefectures, onChange }) => {
   const [ListVisible, setListVisible] = useState<boolean>(true);
-
+  const [checkedPrefectures, setCheckedPrefectures] = useState<string[]>([]);
+  console.log({ checkedPrefectures });
   return (
     <>
-      {ListVisible === false ? (
-        <button className={styles["toggle"]} onClick={() => setListVisible(true)}>
-          一覧を開く
-        </button>
+      {checkedPrefectures.length === 0 ? (
+        <h3 className={styles["unchecked"]}>選択してください</h3>
       ) : (
-        <div>
-          <button className={styles["toggle"]} onClick={() => setListVisible(false)}>
-            一覧を閉じる
-          </button>
-          <div className={classNames(styles.PrefectureList)}>
-            {prefectures.map((prefecture) => (
-              <div className={styles["prefectureCheckbox"]} key={prefecture.prefName}>
-                <input
-                  type="checkbox"
-                  name="Prefecture name"
-                  onChange={(event) =>
-                    onChange(prefecture.prefName, prefecture.prefCode, event.target.checked)
-                  }
-                  id={"checkbox" + prefecture.prefCode}
-                />
-                <label className={styles["text"]} htmlFor={"checkbox" + prefecture.prefCode}>
-                  {prefecture.prefName.length === 3
-                    ? " " + prefecture.prefName
-                    : prefecture.prefName}
-                </label>
-              </div>
-            ))}
-          </div>
+        <div className={styles["checkedList"]}>
+          <h3 className={styles["checked"]}>選択中:</h3> <p>{checkedPrefectures}</p>
         </div>
       )}
+
+      <button className={ListVisible ? styles["toggle"] : ""} onClick={() => setListVisible(true)}>
+        一覧を開く
+      </button>
+      <div className={ListVisible ? "" : styles["toggle"]}>
+        <button onClick={() => setListVisible(false)}>一覧を閉じる</button>
+        <div className={classNames(styles.PrefectureList)}>
+          {prefectures.map((prefecture) => (
+            <div className={styles["prefectureCheckbox"]} key={prefecture.prefName}>
+              <input
+                type="checkbox"
+                name="Prefecture name"
+                onChange={(event) => {
+                  onChange(prefecture.prefName, prefecture.prefCode, event.target.checked);
+
+                  checkedPrefectures.includes(prefecture.prefName + " ")
+                    ? setCheckedPrefectures(
+                        checkedPrefectures.filter(
+                          (checkedPrefectures, index) =>
+                            checkedPrefectures !== prefecture.prefName + " "
+                        )
+                      )
+                    : setCheckedPrefectures([...checkedPrefectures, prefecture.prefName + " "]);
+
+                  // setCheckedList([...checkedList, prefecture.prefName]);
+                }}
+                id={"checkbox" + prefecture.prefCode}
+              />
+              <label className={styles["text"]} htmlFor={"checkbox" + prefecture.prefCode}>
+                {prefecture.prefName.length === 3 ? " " + prefecture.prefName : prefecture.prefName}
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
