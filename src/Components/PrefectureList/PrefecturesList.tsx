29273b7c161b52b4b5474c07fe6ca66e68185e97
schnee25 <schnee.2114@gmail.com>
2022-05-14 21:00:10 +0900

    スマホ表示時にリストの表示非表示を切り替え可能なよう変更

--- src/Components/PrefectureList/PrefecturesList.tsx
+++ src/Components/PrefectureList/PrefecturesList.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./style.module.scss";
+import classNames from "classnames";
 
 type Props = {
   prefectures:
@@ -13,25 +14,40 @@ type Props = {
 
 // 都道府県一覧のチェックボックスを表示するコンポーネント
 const PrefecturesList: React.FC<Props> = ({ prefectures, onChange }) => {
+  const [ListVisible, setListVisible] = useState<boolean>(true);
+
   return (
     <>
-      <div className={styles["PrefectureList"]}>
-        {prefectures.map((prefecture) => (
-          <div className={styles["prefectureCheckbox"]} key={prefecture.prefName}>
-            <input
-              type="checkbox"
-              name="Prefecture name"
-              onChange={(event) =>
-                onChange(prefecture.prefName, prefecture.prefCode, event.target.checked)
-              }
-              id={"checkbox" + prefecture.prefCode}
-            />
-            <label className={styles["text"]} htmlFor={"checkbox" + prefecture.prefCode}>
-              {prefecture.prefName.length === 3 ? " " + prefecture.prefName : prefecture.prefName}
-            </label>
+      {ListVisible === false ? (
+        <button className={styles["toggle"]} onClick={() => setListVisible(true)}>
+          一覧を開く
+        </button>
+      ) : (
+        <div>
+          <button className={styles["toggle"]} onClick={() => setListVisible(false)}>
+            一覧を閉じる
+          </button>
+          <div className={classNames(styles.PrefectureList)}>
+            {prefectures.map((prefecture) => (
+              <div className={styles["prefectureCheckbox"]} key={prefecture.prefName}>
+                <input
+                  type="checkbox"
+                  name="Prefecture name"
+                  onChange={(event) =>
+                    onChange(prefecture.prefName, prefecture.prefCode, event.target.checked)
+                  }
+                  id={"checkbox" + prefecture.prefCode}
+                />
+                <label className={styles["text"]} htmlFor={"checkbox" + prefecture.prefCode}>
+                  {prefecture.prefName.length === 3
+                    ? " " + prefecture.prefName
+                    : prefecture.prefName}
+                </label>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </div>
+      )}
     </>
   );
 };
