79dc44f8f14710d0399e52edd6baa902276ded95
schnee25 <schnee.2114@gmail.com>
2022-05-15 03:34:19 +0900

    ルーター修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import Main from "./pages/Main";
+import React from 'react'
+import Main from './pages/Main'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header style={{ textAlign: "center" }}>
+    <div className='App'>
+      <header style={{ textAlign: 'center' }}>
         <h1>都道府県別人口推移</h1>
       </header>
       <Main />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
