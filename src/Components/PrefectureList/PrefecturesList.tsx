a034cd28562e376b1e018a489439e117be45e46d
schnee25 <schnee.2114@gmail.com>
2022-05-15 02:50:15 +0900

    自動フォーマット走らせてfix

--- src/Components/PrefectureList/PrefecturesList.tsx
+++ src/Components/PrefectureList/PrefecturesList.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import styles from "./style.module.scss";
-import classNames from "classnames";
+import React, { useState } from 'react'
+import styles from './style.module.scss'
+import classNames from 'classnames'
 
 type Props = {
   prefectures:
     | {
-        prefCode: number;
-        prefName: string;
-      }[];
+        prefCode: number
+        prefName: string
+      }[]
 
-  onChange: (name: string, prefName: number, check: boolean) => void;
-};
+  onChange: (name: string, prefName: number, check: boolean) => void
+}
 
 const PrefecturesList: React.FC<Props> = ({ prefectures, onChange }) => {
-  const [ListVisible, setListVisible] = useState<boolean>(true);
-  const [checkedPrefectures, setCheckedPrefectures] = useState<string[]>([]);
+  const [ListVisible, setListVisible] = useState<boolean>(true)
+  const [checkedPrefectures, setCheckedPrefectures] = useState<string[]>([])
 
   return (
     <>
@@ -22,15 +22,15 @@ const PrefecturesList: React.FC<Props> = ({ prefectures, onChange }) => {
         <h3 className={styles.unchecked}>選択してください</h3>
       ) : (
         <div className={styles.checkedList}>
-          <h3 className={styles.checked}>選択中:</h3>{" "}
+          <h3 className={styles.checked}>選択中:</h3>{' '}
           <p className={styles.checkedPrefectures}>{checkedPrefectures}</p>
         </div>
       )}
 
-      <button className={ListVisible ? styles.toggle : ""} onClick={() => setListVisible(true)}>
+      <button className={ListVisible ? styles.toggle : ''} onClick={() => setListVisible(true)}>
         一覧を開く
       </button>
-      <div className={ListVisible ? "" : styles.toggle}>
+      <div className={ListVisible ? '' : styles.toggle}>
         <button onClick={() => setListVisible(false)}>一覧を閉じる</button>
 
         <div className={classNames(styles.PrefectureList)}>
@@ -38,30 +38,30 @@ const PrefecturesList: React.FC<Props> = ({ prefectures, onChange }) => {
             <div
               className={classNames(
                 styles.prefectureCheckboxes,
-                checkedPrefectures.includes(prefecture.prefName + " ") ? styles.CheckedBoxes : null
+                checkedPrefectures.includes(prefecture.prefName + ' ') ? styles.CheckedBoxes : null,
               )}
               key={prefecture.prefName}
             >
               <input
-                type="checkbox"
-                name="PrefectureName"
+                type='checkbox'
+                name='PrefectureName'
                 className={styles.prefectureCheck}
                 onChange={(event) => {
-                  onChange(prefecture.prefName, prefecture.prefCode, event.target.checked);
-                  checkedPrefectures.includes(prefecture.prefName + " ")
+                  onChange(prefecture.prefName, prefecture.prefCode, event.target.checked)
+                  checkedPrefectures.includes(prefecture.prefName + ' ')
                     ? setCheckedPrefectures(
                         checkedPrefectures.filter(
                           (checkedPrefectures, index) =>
-                            checkedPrefectures !== prefecture.prefName + " "
-                        )
+                            checkedPrefectures !== prefecture.prefName + ' ',
+                        ),
                       )
-                    : setCheckedPrefectures([...checkedPrefectures, prefecture.prefName + " "]);
+                    : setCheckedPrefectures([...checkedPrefectures, prefecture.prefName + ' '])
                 }}
-                id={"checkbox" + prefecture.prefCode}
+                id={'checkbox' + prefecture.prefCode}
               />
-              <label className={styles.text} htmlFor={"checkbox" + prefecture.prefCode}>
+              <label className={styles.text} htmlFor={'checkbox' + prefecture.prefCode}>
                 {prefecture.prefName.length === 3
-                  ? "\u00a0 " + prefecture.prefName + "\u2007"
+                  ? '\u00a0 ' + prefecture.prefName + '\u2007'
                   : prefecture.prefName}
               </label>
             </div>
@@ -73,7 +73,7 @@ const PrefecturesList: React.FC<Props> = ({ prefectures, onChange }) => {
         </button>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default PrefecturesList;
+export default PrefecturesList
