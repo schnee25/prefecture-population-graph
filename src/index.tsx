7c57b97ee74a573de3993b3b5058c9f349ce9e2f
schnee25 <schnee.2114@gmail.com>
2022-05-20 00:50:13 +0900

    Route設定

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
 
+const homeUrl = process.env.PUBLIC_URL
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path={homeUrl} element={<App />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 )
