9a473f265f91c5798c19628700c723462463de29
schnee25 <schnee.2114@gmail.com>
2022-05-19 23:15:15 +0900

    ルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Main from './pages/Main'
+import { Route, Routes } from 'react-router-dom'
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div className='App'>
       <header style={{ textAlign: 'center' }}>
         <h1>都道府県別人口推移</h1>
       </header>
-      <Main />
+      <Routes>
+        <Route path='/' element={<Main />} />
+      </Routes>
     </div>
   )
 }
