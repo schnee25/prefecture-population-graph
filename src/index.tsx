d9634d9ed965114590e895ccc34a488a60244ef9
schnee25 <schnee.2114@gmail.com>
2022-05-21 00:24:28 +0900

    コメントアウト削除

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-const homeUrl = process.env.PUBLIC_URL
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
